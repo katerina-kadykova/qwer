95cd72acfa984baf4edaab122c23eb547e58afc7
katerina-kadykova <150951511+katerina-kadykova@users.noreply.github.com>
2023-11-21 13:46:20 +0400

    app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ tg.MainButton.color = "#2cab37";
 let item = "";
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
 
 tg.MainButton.setText("Вывести информацию по чикенбургеру");
 
@@ -27,6 +28,17 @@ btn2.addEventListener("click", function () {
   else {
     tg.MainButton.setText("Вывести информацию по чикен премьеру");
     item = "2";
+    tg.MainButton.show();
+  }
+});
+
+btn3.addEventListener("click", function () {
+  if (tg.MainButton.isVisible) {
+    tg.MainButton.hide();
+  }
+  else {
+    tg.MainButton.setText("Вывести информацию по биг спешиалу");
+    item = "3";
     tg.MainButton.show();
   }
 });
